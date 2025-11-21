d8f5171f774fd151dfb515c9ecb79ef2e9805d56
Olivier Carignan <olive.carignan@gmail.com>
2025-11-20 20:46:39 -0500

    remove netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,12 +10,8 @@ import node from "@astrojs/node";
 
 import react from "@astrojs/react";
 
-import netlify from "@astrojs/netlify";
-
 // https://astro.build/config
 export default defineConfig({
-  adapter: netlify(),
-
   vite: {
     plugins: [
       svgr({
